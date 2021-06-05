e295b63adcc0f3da80e3265d002d534dc92c6a79
Danijel Gomola <84811407+gomke235@users.noreply.github.com>
2021-06-05 23:02:18 +0200

    Add files via upload

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,12 @@
 const uključiNavigaciju = document.querySelector('.nav-btn');
 const navLinkovi = document.querySelectorAll('.nav-link');
 
+// Funkcija za otvaranje navigacione liste
 uključiNavigaciju.addEventListener('click', () => {
     document.body.classList.toggle('nav-otvori');
 });
 
+// Funckija za zatvaranje navigacione liste
 navLinkovi.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-otvori');
